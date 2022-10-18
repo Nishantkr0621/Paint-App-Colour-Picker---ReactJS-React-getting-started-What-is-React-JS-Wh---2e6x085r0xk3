dc86b4b8f556a54e749d291072a012a78c12fa76
Nishant Kumar <103330522+Nishantkr0621@users.noreply.github.com>
2022-10-18 15:51:22 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,13 @@
+import React from 'react' ;
 
+const Selection = (props) => {
+  return(
+    <>
+      <div  className="fix-box" style={{backgroundColor : props.applyColor}}>
+        <h2 className='subheading' >Selection</h2>
+
+      </div>
+    </>
+  );
+}
+export default Selection ;
